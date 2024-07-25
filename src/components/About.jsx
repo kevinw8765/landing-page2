fb772c54a7764e67f9f451bc8622cb8a9a9da4b8
Kevin Wong <kevinw8765@gmail.com>
2024-07-25 01:44:18 -0400

    followed

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name = "about" className = "w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div>
+            <p>About</p>
+        </div>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Iste itaque omnis esse iusto, repellat, cum sit accusamus, 
+            est nobis dolor porro asperiores odio eveniet quod cupiditate?
+            Voluptatum neque iste minima.
+        </p>
+
+        <br/>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Repellat minima, porro molestias culpa ipsum vel libero cum omnis, 
+            magnam maiores iusto recusandae facere ratione cumque dicta? 
+            Ea accusantium iusto incidunt?
+        </p>
+    </div>
+  )
+}
+
+export default About
